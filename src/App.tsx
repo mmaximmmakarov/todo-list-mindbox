98eae7ad965e819cf25032fb3f70e5423bb9d0d3
mmaximmmakarov <mmaximmmakarov@yandex.ru>
2025-01-22 11:23:30 +0300

    improves code

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,7 @@ const App: React.FC = () => {
         } else {
             return true;
         }
-    }), [ todos ]);
+    }), [ todos, filter ]);
 
     return (
         <div className="app-container">
